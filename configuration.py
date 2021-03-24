615160ddda8b2284114a4f196600f92e2858b49d
Hiszpan_ <wojtek.szacon@gmail.com>
2021-03-24 12:23:16 +0100

    So I think menu is configured almost, now I am gonna be adding randomPieChart function

--- configuration.py
+++ configuration.py
@@ -1,2 +1,13 @@
 randomDataMin = 1
 ramdomDataMax = 2137
+
+#I will later on add other countries
+listOfCountries = [
+    "Poland",
+    "Great Britian",
+    "India",
+    "North Korea",
+    "United States of America",
+    "Japan",
+    "Austria"
+]
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def randomPieChart(title):
     print("wow")
 
