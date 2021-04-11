8a472542160df437be3050c614a748bacba76cf8
HiszpanInk <48619227+HiszpanInk@users.noreply.github.com>
2021-04-11 14:51:54 +0200

    Deleted side.py

--- side.py
+++
@@ -1,6 +0,0 @@
-#sideprogram for generating not random charts
-selectedIndexes = ["a", "b"]
-df = pd.DataFrame({title: data})
-plot = df.plot.pie(y=title, figsize=(11, 11), labels="selectedIndexes", fontsize=12, legend=False, autopct='%1.1f%%')
-fig = plot.get_figure()
-fig.savefig("myplot.png")
